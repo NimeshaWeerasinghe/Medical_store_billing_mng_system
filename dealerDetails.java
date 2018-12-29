38c12bba5f2c0520925adee582a9d83ea6201ec2
NimeshaWeerasinghe <nimeshaweerasinghe94@gmail.com>
2018-12-29 13:22:27 +0530

    dealer details

---
+++ dealerDetails.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package msbms;
+
+/**
+ *
+ * @author Hansi
+ */
+public class dealerDetails {
+    private int dea_ID;
+    private String dea_name;
+    private String address;
+    private int mob_num;
+    private String city;
+    private String email;
+    private int co_ID;
+
+    /**
+     * @return the dea_ID
+     */
+    public int getDea_ID() {
+        return dea_ID;
+    }
+
+    /**
+     * @param dea_ID the dea_ID to set
+     */
+    public void setDea_ID(int dea_ID) {
+        this.dea_ID = dea_ID;
+    }
+
+    /**
+     * @return the dea_name
+     */
+    public String getDea_name() {
+        return dea_name;
+    }
+
+    /**
+     * @param dea_name the dea_name to set
+     */
+    public void setDea_name(String dea_name) {
+        this.dea_name = dea_name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the mob_num
+     */
+    public int getMob_num() {
+        return mob_num;
+    }
+
+    /**
+     * @param mob_num the mob_num to set
+     */
+    public void setMob_num(int mob_num) {
+        this.mob_num = mob_num;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the co_ID
+     */
+    public int getCo_ID() {
+        return co_ID;
+    }
+
+    /**
+     * @param co_ID the co_ID to set
+     */
+    public void setCo_ID(int co_ID) {
+        this.co_ID = co_ID;
+    }
+}
